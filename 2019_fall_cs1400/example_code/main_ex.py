f29a390eea8db60e64a026e41d854fd4eb346371
Drew Adams <drew@drewtadams.com>
2019-08-28 18:04:42 -0600

    added main() example and additional strings example

---
+++ 2019_fall_cs1400/example_code/main_ex.py
@@ -0,0 +1,15 @@
+import math
+
+
+def main():
+    print('hello world')
+    
+    x = 4/3
+    y = x * math.pi
+    print(str(y))
+    
+    (2 + 3) * 4
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2019_fall_cs1400/example_code/strings2.py
@@ -0,0 +1,11 @@
+# import
+# def
+# if
+# elif
+
+x = ('-'*25) + '\n'
+y = (x)*4
+print(f'hello foobar\n{y}')
+
+exp_month = 'january'
+print(f'this expires in {exp_month}')
