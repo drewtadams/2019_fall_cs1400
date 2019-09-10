612fd25e871f200aadf53082fb206dbccc7ef608
Drew Adams <drew@drewtadams.com>
2019-09-09 18:18:57 -0600

    added loops and updated main_ex

---
+++ 2019_fall_cs1400/example_code/loops.py
@@ -0,0 +1,56 @@
+import math
+
+def main():
+    for x in range(1,11,2):
+        print(x)
+    
+    x = ('-')*10
+    print(f'{x}')
+    
+    x = 0
+    y = False
+    while x != 10:
+        print(x)
+        x += 1
+        
+        #if x == 10:
+        #    x += 1
+    
+    x = 'foobar'
+    for letter in x:
+        print(letter, end='')
+        print('')
+        
+    x = [1,2,3,4,5,6]
+    for item in x:
+        print(item)
+        
+    x = list(range(10))
+    print(x)
+    
+    
+    x = range(10,-1,-1)
+    for value in x:
+        print(value)
+        
+    print('\n\n\n')
+        
+    for foo in range(10):
+        print('%-3d|%10d|%10d' % (foo, 10**foo, foo*2))
+        
+    for x in range(10):
+        val = x * math.pi
+        print('your salary is $%0.2f' % val)
+        
+    x = float(input('enter some int: '))
+    print(x)
+    if x == 1:
+        print('done')
+    elif x == 2:
+        print('bar')
+    else:
+        print('foo')
+
+
+if __name__ == '__main__':
+    main()
--- 2019_fall_cs1400/example_code/main_ex.py
+++ 2019_fall_cs1400/example_code/main_ex.py
@@ -1,6 +1,19 @@
 import math
 
 
+def count(start=1, end=10):
+    for x in range(start, end, 5):
+        print(x)
+        break
+        
+    y = 5
+    while y <= 10:
+        print('still true', end=' ')
+        y += 1
+        
+    print('\\nthe end', end=' ')
+
+
 def main():
     print('hello world')
     
@@ -8,7 +21,7 @@ def main():
     y = x * math.pi
     print(str(y))
     
-    (2 + 3) * 4
+    count(1, 100)
 
 
 if __name__ == '__main__':
